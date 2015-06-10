d64dc63e4cfde0c0fb0f30fbe886bd6b85916925
Justin <justin@c7sanu7E.lan>
2015-06-10 14:56:34 -0400

    added to-do input to FT

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import unittest
+from selenium.webdriver.common.keys import Keys
 
 class NewVisitorTest(unittest.TestCase):
 	def setUp(self):
@@ -13,16 +14,27 @@ class NewVisitorTest(unittest.TestCase):
 		#User goes to homepage
 		self.browser.get('http://localhost:8000')
 		
-		#Sees to-do in the title
+		#Sees to-do in the title and header
 		self.assertIn('To-Do', self.browser.title)
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
 
 		#Invited to enter a to-do item on the homepage
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
 		#User types "Buy feathers" into a text box
+		inputbox.send_keys('Buy peacock feathers')
 
 		#Page updates on enter and lists "1: Buy Feathers" as an item in the list
+		inputbox.send_keys(Keys.ENTER)
+		
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue( any(row.text == '1:Buy peacock feathers' for row in rows) )
 
 		#There is still a text box inviting an additional item
+		self.fail('to-finish')
 
 		#User enters "Use feathers"
 
