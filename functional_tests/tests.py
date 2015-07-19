2ab5e1f01c2ec597c2e7396f85214a239688261b
Justin <localtalent@gmail.com>
2015-07-19 00:16:29 -0400

    layout tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -101,7 +101,20 @@ class NewVisitorTest(LiveServerTestCase):
 
 		#User visits URL and sees to-do list is still there
 		
-
+	#---------------------
+	#  start and retrieve
+	#---------------------
+		
+	def test_layout_and_styling(self):
+		self.browser.get(self.live_server_url)
+		self.browser.set_window_size(1024,768)
+		
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width']/2, 512, delta=5)
+		
+		inputbox.send_keys('testing\n')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width']/2, 512, delta=5)
 
 
 
