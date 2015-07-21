488a63833ee676c90c54ecf530fa97959a1c025b
Justin <localtalent@gmail.com>
2015-07-20 20:55:38 -0400

    move sqlite db outside source tree

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,8 +4,24 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from lists.models import Item
 
+import sys
 
 class NewVisitorTest(StaticLiveServerTestCase):
+	
+	@classmethod
+	def setUpClass(cls):
+		for arg in sys.argv:
+			if 'liveserver' in arg:
+				cls.server_url = 'http://' + arg.split('=')[1]
+				return
+		super().setUpClass()
+		cls.server_url = cls.live_server_url
+		
+	@classmethod	
+	def tearDownClass(cls):
+		if cls.server_url == cls.live_server_url:
+			super().tearDownClass()
+			
 	def setUp(self):
 		self.browser = webdriver.Firefox()
 		#self.browser.implicitly_wait()
@@ -38,7 +54,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		#User goes to homepage
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		
 		#Sees to-do in the title and header
 		self.assertIn('To-Do', self.browser.title)
@@ -76,7 +92,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		self.browser.quit()
 		self.browser = webdriver.Firefox()
 		
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		page_text = self.browser.find_element_by_tag_name('body').text
 		
 		self.assertNotIn('Buy peacock feathers', page_text)
@@ -106,7 +122,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 	#---------------------
 		
 	def test_layout_and_styling(self):
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		self.browser.set_window_size(1024,768)
 		
 		inputbox = self.browser.find_element_by_id('id_new_item')
